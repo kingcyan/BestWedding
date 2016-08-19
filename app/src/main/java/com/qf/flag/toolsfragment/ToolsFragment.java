df94f662817f8b88b9e4ce525082c96bc12b0ba1
King <king_syan@163.com>
2016-08-19 21:02:08 +0800

    添加adapter包。可以显示Tool页面头部

--- app/src/main/java/com/qf/flag/toolsfragment/ToolsFragment.java
+++ app/src/main/java/com/qf/flag/toolsfragment/ToolsFragment.java
@@ -1,9 +1,9 @@
 package com.qf.flag.toolsfragment;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.qf.bestwedding.MyApplication;
 import com.qf.bestwedding.R;
 import com.qf.entity.ToolEntity;
@@ -19,6 +19,7 @@ import retrofit2.Response;
  */
 public class ToolsFragment extends BaseFragment {
     ImageView ToolHeadIMG;
+
     @Override
     protected int getContentView() {
         return R.layout.fragment_tools;
@@ -26,19 +27,25 @@ public class ToolsFragment extends BaseFragment {
 
     @Override
     protected void init(View view) {
+        setToolIMG(view);
+    }
+
+    private void setToolIMG(View view) {
         ToolHeadIMG = (ImageView) view.findViewById(R.id.tool_head_img);
         Call<ToolEntity> call = MyApplication.utils.getToolIMG();
         call.enqueue(new Callback<ToolEntity>() {
             @Override
             public void onResponse(Call<ToolEntity> call, Response<ToolEntity> response) {
-//                Glide.with(getActivity())
-//                        .load(response.body().getData()
-//                                .getFloors().getTOOLS_BANNER().getHoles().get(0).
-//                                        getPosters().getImage_path())
-//                        .into(ToolHeadIMG);
-                Log.e("TAG", response.body().getData()
-                        .getFloors().getTOOLS_BANNER().getHoles().get(0).
-                                getPosters().getImage_path() );
+                //打印得到的图片地址
+                //                Log.e("TAG", response.body().getData()
+                //                        .getFloors().getTOOLS_BANNER().getHoles().get(0).
+                //                                getPosters().getImage_path() );
+                //将图片设置给控件
+                Glide.with(getActivity())
+                        .load(response.body().getData()
+                                .getFloors().getTOOLS_BANNER().getHoles().get(0).
+                                        getPosters().getImage_path())
+                        .into(ToolHeadIMG);
             }
 
             @Override
