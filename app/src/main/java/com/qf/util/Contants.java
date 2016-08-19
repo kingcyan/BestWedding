124e212e014bd4d1fca01c17e8929aa5abcc8954
King <king_syan@163.com>
2016-08-19 19:06:09 +0800

    常量类，工具类，解析Json类的创建

---
+++ app/src/main/java/com/qf/util/Contants.java
@@ -0,0 +1,9 @@
+package com.qf.util;
+
+/**
+ * Created by King
+ * 2016/8/19
+ */
+public interface Contants {
+    String TOOL_HEAD_IMG = "http://www.hunliji.com/p/wedding/index.php/home/APIPosterBlock/block_info?id=1004&app_version=6.8.7&city=0";
+}
---
+++ app/src/main/java/com/qf/util/JsonUtil.java
@@ -0,0 +1,8 @@
+package com.qf.util;
+
+/**
+ * Created by King
+ * 2016/8/19
+ */
+public class JsonUtil {
+}
