de1e2bd987f92eff50e21d544ca7ad90a94cb89d
King <king_syan@163.com>
2016-08-19 20:40:19 +0800

    常量类，工具类，解析Json类的创建

--- app/src/main/java/com/qf/util/Contants.java
+++ app/src/main/java/com/qf/util/Contants.java
@@ -8,5 +8,6 @@ public interface Contants {
 
     String HTTP_ROOT   = "http://www.hunliji.com/";
     //工具页面顶部的图片
+
     String TOOL_HEAD_IMG = "p/wedding/index.php/home/APIPosterBlock/block_info?id=1004&app_version=6.8.7&city=0";
 }
